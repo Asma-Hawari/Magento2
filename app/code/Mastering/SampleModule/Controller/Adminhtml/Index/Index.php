e5af2604dbcddc91da46288c9742e33b8ba9e5d7
Asma-Hawari <Asma.Hawari.AH@gmail.com>
2019-06-17 11:30:42 +0300

    Problem Solved By Removeing layout tag from layout.xml

--- app/code/Mastering/SampleModule/Controller/Adminhtml/Index/Index.php
+++ app/code/Mastering/SampleModule/Controller/Adminhtml/Index/Index.php
@@ -5,12 +5,14 @@ namespace Mastering\SampleModule\Controller\Adminhtml\Index;
 use Magento\Backend\App\Action;
 use Magento\Framework\Controller\Result\Raw;
 use Magento\Framework\Controller\ResultFactory;
+Use Magento\Framework\Debug;
 
 class Index extends Action
 {
 
     public function execute()
     {
+
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
